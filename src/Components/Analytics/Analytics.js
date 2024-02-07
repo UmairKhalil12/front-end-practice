11b1940772e62cc5f7cf7880a886a2d62d1c1918
Umair Khalil <khalil.umair21@gmail.com>
2024-02-07 11:20:36 +0500

    commit-3

--- src/Components/Analytics/Analytics.js
+++ src/Components/Analytics/Analytics.js
@@ -2,22 +2,18 @@ import "./Analytics.css"
 import laptop from '../../assets/laptop.jpg'
 
 
-export default function Analytics({ MobileNavStatus , windowWidth}) {
-    const containerClassName = MobileNavStatus && windowWidth < 768 ? 'analytics-main-open': 'analytics-main'
-
+export default function Analytics() {
 
     return (
         <div className='analytics-main'>
 
             <div className="analytics-with-image">
-                <div>
-                    <img src={laptop} className="laptop-image" alt="laptop" />
-                </div>
+                <img src={laptop} className="laptop-image" alt="laptop" />
             </div>
 
             <div className="analytics-second">
+                <div>
 
-                <div className="analytics-second-2">
                     <p className="analytics-para" >MERN DASHBOARD</p>
                     <h1>Mastering Mern </h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -25,13 +21,11 @@ export default function Analytics({ MobileNavStatus , windowWidth}) {
                         ad porro perferendis illo voluptatem sint temporibus,
                         vitae, sit nemo aliquid doloribus impedit assumenda? Repellendus, amet?</p>
 
-
                     <div className="analytics-btn-div">
                         <button className="analytics-btn"> Get Started </button>
                     </div>
 
                 </div>
-
             </div>
         </div>
     )
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,33 +1,72 @@
 import './Footer.css'
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare,
+} from 'react-icons/fa';
 
 
-export default function () {
+export default function Footer() {
     return (
-        <div className="main-footer">
+        <div className='main-footer'>
 
-            <div className="inside-footer">
-                <h1 className="heading-footer">REACT</h1>
+            <div className="main-footer-2">
 
-                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Quis, eligendi aliquam facilis suscipit, nostrum nam, 
-                ullam autem fugit iure impedit odio harum voluptate 
-                corrupti nobis labore iusto reiciendis illum optio? </p>
+                <div className="inside-footer">
+                    <h1 className="heading-footer">REACT</h1>
+
+                    <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Quis, eligendi aliquam facilis suscipit, nostrum nam,
+                        ullam autem fugit iure impedit odio harum voluptate
+                        corrupti nobis labore iusto reiciendis illum optio? </p>
+
+                    <div className='footerLogo'>
+                        <FaFacebookSquare className='logos' />
+                        <FaInstagram className='logos' />
+                        <FaTwitterSquare className='logos' />
+                        <FaGithubSquare className='logos' />
+                        <FaDribbbleSquare className='logos' />
+                    </div>
+
+                </div>
 
             </div>
 
-            <div className="inside-footer">
+            <div className='main-footer-3'> 
 
-            </div>
+                <div className="inside-footer-2">
 
-            <div className="inside-footer">
+                    <h4>Solution</h4>
+                    <p>Analysis</p>
+                    <p>Marketting</p>
+                    <p>Commerce</p>
+                    <p>Insights</p>
 
-            </div>
+                </div>
 
-            <div className="inside-footer">
+                <div className="inside-footer-2">
+                    <h4>Support</h4>
+                    <p>Pricing</p>
+                    <p>Documentation</p>
+                    <p>Guide </p>
+                    <p>API status</p>
+
+                </div>
+
+                <div className="inside-footer-2">
+                    <h4>Legal</h4>
+                    <p>Claim</p>
+                    <p>Policy</p>
+                    <p>Terms</p>
+                </div>
 
             </div>
 
         </div>
+
+
 
     );
 
